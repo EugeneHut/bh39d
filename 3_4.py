d7d738b29a2e89281f85f57e28420cf10a34e5df
EugeneHut <anotherappshut@gmail.com>
2023-06-14 15:52:01 +0300

    ///

--- 3_4.py
+++ 3_4.py
@@ -1,5 +1,5 @@
 numbers = input("Введите 3 числа:")
 neg_num = numbers.count('-')
 pos_num = 3 - neg_num
-print(f"Положительных счел - {pos_num}")
+print(f"Положительных чисел - {pos_num}")
 print(f"Отрицательных чисел - {neg_num}")
